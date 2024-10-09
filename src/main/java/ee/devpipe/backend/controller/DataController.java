5a04bdc1b8edb0a3b6930096e97a952dfae938cd
jaanus <zaar2213@gmail.com>
2024-10-10 00:32:30 +0300

    started java maven spring boot project to test

---
+++ src/main/java/ee/devpipe/backend/controller/DataController.java
@@ -0,0 +1,27 @@
+package ee.devpipe.backend.controller;
+
+import ee.devpipe.backend.model.Data;
+import ee.devpipe.backend.repository.DataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/data")
+public class DataController {
+
+    @Autowired
+    private DataRepository dataRepository;
+
+    @PostMapping
+    public String saveData(@RequestBody Data data) {
+        dataRepository.save(data);
+        return "Data saved!";
+    }
+
+    @GetMapping
+    public List<Data> getAllData() {
+        return dataRepository.findAll();
+    }
+}
---
+++ src/test/java/ee/devpipe/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.devpipe.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
